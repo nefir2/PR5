da3267ff621159aea5b5605f2391e6409066da16
nefir2 <vadim.ahtamov@gmail.com>
2023-05-17 05:37:51 +0300

    Добавьте файлы проекта.

---
+++ PR5/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PR5
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private Random random = new Random();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void runawayButton_MouseEnter(object sender, MouseEventArgs e)
+		{
+			double newX = random.Next(0, (int)(this.ActualWidth - runawayButton.ActualWidth));
+			double newY = random.Next(0, (int)(this.ActualHeight - runawayButton.ActualHeight));
+
+			DoubleAnimation xAnimation = new DoubleAnimation(newX, TimeSpan.FromSeconds(1));
+			DoubleAnimation yAnimation = new DoubleAnimation(newY, TimeSpan.FromSeconds(1));
+
+			translateTransform.BeginAnimation(TranslateTransform.XProperty, xAnimation);
+			translateTransform.BeginAnimation(TranslateTransform.YProperty, yAnimation);
+		}
+	}
+}
